313f98d5873ca41fafc443fd719f1c1708122d2b
Andrew <andrewbrodriguez@gmail.com>
2023-08-26 14:56:45 -0400

    Basic Clustering and writing the Clusters

--- backend/ai/curator/clusterer.py
+++ backend/ai/curator/clusterer.py
@@ -3,9 +3,10 @@ from sklearn.preprocessing import OneHotEncoder
 from keras.preprocessing.text import Tokenizer
 from sklearn.cluster import KMeans
 
-df = pd.read_csv("backend/ai/curator/clusterer.py", encoding = "ISO-8859-1")
-with open('backend/ai/curator/20k.txt') as f:
+df = pd.read_csv("/Users/andrewrodriguez/Desktop/debrief/backend/ai/curator/article_stats.csv")
+with open('/Users/andrewrodriguez/Desktop/debrief/backend/ai/curator/20k.txt') as f:
     words = f.readlines()
+    
 corpus = []
 for word in words:
     corpus.append([word[:-1]])
@@ -15,23 +16,22 @@ tokenizer.fit_on_texts(corpus)
 titleTokenizer = Tokenizer()
 titleCorpus = []
 for index, row in df.iterrows():
-    titleCorpus.append([row["Heading"]])
+    titleCorpus.append([row["article_title"]])
 titleTokenizer.fit_on_texts(titleCorpus)
 
-titleOHEs = titleTokenizer.texts
 
-matrices = tokenizer.texts_to_matrix(df['Article'], mode = 'count')
+matrices = tokenizer.texts_to_matrix(df['article_summary'], mode = 'count')
 ohe = []
-for i in range(2692):
+for i in range(454):
     ohe.append(matrices[i][0:])
 
 # Perform kmean clustering
-num_clusters = 100
+num_clusters = 50
 clustering_model = KMeans(n_clusters=num_clusters)
 clustering_model.fit(ohe)
 cluster_assignment = clustering_model.labels_
 
-articles = df["Article"]
+articles = df["article_summary"]
 
 article_clusters = [[] for i in range(num_clusters)]
 for article_id, cluster_id in enumerate(cluster_assignment):
